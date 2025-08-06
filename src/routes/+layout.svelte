d17828a2dd5fa59b2b0d27cd7022497f36b37b54
deltea <thcheetah777@gmail.com>
2025-08-06 16:53:27 -0700

    make lighthoouse happy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 <svelte:head>
   <title>@deltea</title>
 	<link rel="icon" href={favicon} />
+  <meta name="description" content="hi! i'm leo, and this is my personal website where i put the things i make, like games, web apps, and other random stuff!" />
 </svelte:head>
 
 {@render children?.()}
