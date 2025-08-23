12e40f47b733a1b21b43d369cb5930048e5db350
deltea <thcheetah777@gmail.com>
2025-08-22 22:26:32 -0700

    update favicon to be better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import "../app.css";
   import "iconify-icon";
-	import favicon from "$lib/assets/favicon.svg";
+	// import favicon from "$lib/assets/favicon.svg";
   import pfp from "$lib/assets/pfp.jpg";
 
 	let { children } = $props();
@@ -9,7 +9,7 @@
 
 <svelte:head>
   <title>@deltea</title>
-	<link rel="icon" href={favicon} />
+	<!-- <link rel="icon" href={favicon} /> -->
   <meta name="description" content="hi! i'm leo, and this is my personal website where i put the things i make, like games, web apps, and other random stuff!" />
 </svelte:head>
 
