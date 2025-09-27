11b9f90b6b6af80b02598e44ccaad8246d4aaab7
deltea <thcheetah777@gmail.com>
2025-09-26 19:37:50 -0700

    only neko on desktop

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,25 @@
   import "iconify-icon";
 	// import favicon from "$lib/assets/favicon.svg";
   import pfp from "$lib/assets/vibing.png";
+  import banner from "$lib/assets/banner.jpg"
   import { oneko } from "$lib/oneko";
   import { onMount } from "svelte";
 
 	let { children } = $props();
 
-  onMount(() => oneko());
+  function isMobileUserAgentData() {
+    if (navigator.userAgent) {
+      console.log(navigator.userAgent.toLowerCase());
+      return navigator.userAgent.toLowerCase().includes("mobile");
+    }
+    return false; // Fallback for browsers without support
+  }
+
+  onMount(() => {
+    if (!isMobileUserAgentData()) {
+      oneko();
+    }
+  });
 </script>
 
 <svelte:head>
@@ -20,11 +33,17 @@
 <div class="min-h-screen flex justify-center py-8">
   <main class="w-full max-w-3xl flex flex-col items-start px-4">
     <!-- banner -->
-    <div class="grid-background border-2 border-fg w-full h-24"></div>
+    <div class="grid-background border-2 border-fg w-full h-30"></div>
+    <!-- <div class="w-full h-30 p-4 border-2 border-fg">
+      <div
+        class="bg-cover bg-[center_bottom_-4rem] w-full h-full"
+        style:background-image="url({banner})"
+      ></div>
+    </div> -->
 
     <div class="flex gap-6 sm:-translate-y-8 -translate-y-4 sm:translate-x-8 translate-x-6">
       <!-- pfp -->
-      <a aria-label="home" href="/" class="block sm:size-28 size-20 rounded-full outline-3 outline-fg">
+      <a aria-label="home" href="/" class="block sm:size-28 size-20 rounded-full outline3 outline-fg">
         <div
           class="bg-cover bg-center size-full rounded-full outline-[16px] outline-bg"
           style:background-image="url({pfp})"
