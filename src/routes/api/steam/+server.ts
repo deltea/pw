f577b3af57e9de1cf2aac7c5afc1b24ca7dc9ccd
deltea <thcheetah777@gmail.com>
2025-11-24 21:51:40 -0600

    games blacklisting

--- src/routes/api/steam/+server.ts
+++ src/routes/api/steam/+server.ts
@@ -1,9 +1,15 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
 import { PUBLIC_STEAM_API_KEY } from "$env/static/public";
 
+const gamesBlacklist = ["Aseprite"];
+
 export const GET: RequestHandler = async () => {
-  const response = await fetch(`https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${PUBLIC_STEAM_API_KEY}&steamid=76561199405706388&count=1`);
+  const response = await fetch(`https://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v1/?key=${PUBLIC_STEAM_API_KEY}&steamid=76561199405706388&count=2`);
   const data = await response.json();
 
-  return json(data.response.games[0]);
+  return json(
+    gamesBlacklist.includes(data.response.games[0].name) ?
+    data.response.games[1] :
+    data.response.games[0]
+  );
 }
