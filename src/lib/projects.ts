f48bf1ae6002b87ae8b2e5a307f2dc138b763291
deltea <thcheetah777@gmail.com>
2025-12-25 18:14:15 -0800

    add a simple now page

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,106 +1,88 @@
-export const projects = [
-    // ------ game projects ------
-    {
-      title: "power cut",
-      type: "game",
-      description: "a short puzzle platformer made for a game jam",
-      link: "https://deltea.itch.io/power-cut",
-      source: "https://github.com/deltea/power-cut"
-    },
-    {
-      title: "catalyst",
-      type: "game",
-      description: "a bullet hell boss rush game made for a month long game jam",
-      link: "https://deltea.itch.io/catalyst",
-      source: "https://github.com/deltea/brj-exchange"
-    },
-    {
-      title: "jetcat",
-      type: "game",
-      description: "a fast-paced speedrun platformer with a cat that can fly",
-      link: "https://deltea.itch.io/jetcat",
-      source: "https://github.com/deltea/Gravity"
-    },
-    {
-      title: "g r a p p l e",
-      type: "game",
-      description: "grappling in the 90s",
-      link: "https://deltea.itch.io/grapple",
-    },
-    {
-      title: "rpgeometry",
-      type: "game",
-      description: "a horde shooter made for bullet hell game jam 2023",
-      link: "https://deltea.itch.io/rpgeometry",
-    },
-    {
-      title: "long shot",
-      type: "game",
-      description: "an abstract shooter with a weird movement mechanic",
-      link: "https://deltea.itch.io/long-shot",
-    },
+export enum STATUS { DONE, DOING, DRAFT };
 
-    // ------ web projects ------
-    {
-      title: "peridot",
-      type: "web",
-      description: "obsidian meets pinterest, store your ideas locally",
-      link: "https://peridot.deltea.space/",
-      source: "https://github.com/deltea/peridot",
+export const projects = {
+  "web": {
+    "peridot": {
+      status: STATUS.DOING,
+      description: "pinterest meets obsidian, store ideas locally",
+      link: "https://peridot.deltea.space",
+      source: "https://github.com/deltea/peridot"
     },
-    {
-      title: "downbeat",
-      type: "web",
+    "downbeat": {
+      status: STATUS.DOING,
       description: "a small tool for syncing gifs to the beat",
       link: "https://downbeat.deltea.space/",
       source: "https://github.com/deltea/downbeat"
     },
-    {
-      title: "the movie game",
-      type: "web",
+    "the movie game": {
+      status: STATUS.DONE,
       description: "higher or lower game, but with movies",
       link: "https://movie-game.deltea.space/",
       source: "https://github.com/deltea/themoviegame",
     },
-    {
-      title: "button maker",
-      type: "web",
+    "button maker": {
+      status: STATUS.DONE,
       description: "a small tool to make 88x31 buttons",
       link: "https://buttons.deltea.space/",
       source: "https://github.com/deltea/button-maker",
     },
-    {
-      title: "youguess",
-      type: "web",
+    "youguess": {
+      status: STATUS.DONE,
       description: "higher or lower game, but with youtube videos",
       link: "https://youguess.deltea.space/",
       source: "https://github.com/deltea/youguess",
     },
-    // {
-    //   title: "atlas academy",
-    //   type: "web",
-    //   description: "a blog made to document my travels",
-    //   link: "https://atlasacademy.blog/",
-    //   source: "https://github.com/deltea/atlasacademy.blog",
-    // },
-    {
-      title: "color pong",
-      type: "web",
+    "color pong": {
+      status: STATUS.DONE,
       description: "a cool looking screensaver-ahh pong simulation",
       link: "https://color-pong.deltea.space/",
       source: "https://github.com/deltea/color-pong",
+    }
+  },
+  "games": {
+    "power cut": {
+      status: STATUS.DONE,
+      description: "a short puzzle platformer made for a game jam",
+      link: "https://deltea.itch.io/power-cut",
+      source: "https://github.com/deltea/power-cut"
     },
-
-    // ------ hardware projects ------
-    {
-      title: "yume",
-      type: "hardware",
+    "catalyst": {
+      status: STATUS.DONE,
+      description: "a bullet hell boss rush game made for a month long game jam",
+      link: "https://deltea.itch.io/catalyst",
+      source: "https://github.com/deltea/brj-exchange"
+    },
+    "jetcat": {
+      status: STATUS.DONE,
+      description: "a fast-paced speedrun platformer with a cat that can fly",
+      link: "https://deltea.itch.io/jetcat",
+      source: "https://github.com/deltea/Gravity"
+    },
+    "g r a p p l e": {
+      status: STATUS.DONE,
+      description: "grappling in the 90s",
+      link: "https://deltea.itch.io/grapple",
+    },
+    "rpgeometry": {
+      status: STATUS.DONE,
+      description: "a horde shooter made for bullet hell game jam 2023",
+      link: "https://deltea.itch.io/rpgeometry",
+    },
+    "long shot": {
+      status: STATUS.DONE,
+      description: "an abstract shooter with a weird movement mechanic",
+      link: "https://deltea.itch.io/long-shot",
+    }
+  },
+  "hardware": {
+    "yume": {
+      status: STATUS.DOING,
       description: "a custom mp3 player with an oled display and rotary encoder",
       link: "https://github.com/deltea/yume_os",
     },
-    {
-      title: "naicha68",
-      type: "hardware",
+    "naicha68": {
+      status: STATUS.DRAFT,
       description: "a 68% gasket-mounted mechanical keyboard with custom pcb"
     }
-  ];
+  }
+}
---
+++ src/routes/now/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Header from "$components/Header.svelte";
+    import { projects } from "$lib/projects.js";
+
+  let { data } = $props();
+</script>
+
+<svelte:head>
+  <title>@deltea - now</title>
+</svelte:head>
+
+<Header
+  title="now"
+  description="stuff i'm currently working on or doing"
+/>
+
+<p>currently working on:</p>
