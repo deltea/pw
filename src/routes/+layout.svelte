ff4c2d8e1a1cb4c9de436c1776ddcd821df12ff0
deltea <thcheetah777@gmail.com>
2026-02-17 15:01:40 -0800

    add crt effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,7 +23,7 @@
   <meta name="description" content="hi! i'm leo, and this is my personal website where i put the things i make, like games, web apps, and other random stuff!" />
 </svelte:head>
 
-<div class="min-h-screen flex justify-center py-8">
+<div class="min-h-screen flex justify-center py-8 crt">
   <main class="w-full max-w-3xl flex flex-col items-start px-4">
     <!-- main page content -->
     <div class="grow w-full flex flex-col">
