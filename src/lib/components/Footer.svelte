7fffe5a37f319b5a74ef498046f553f08eae6673
deltea <thcheetah777@gmail.com>
2026-02-19 00:00:55 -0800

    add hack club webring to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,4 @@
 <footer class="font-bold mt-16 flex sm:justify-between justify-center items-center w-full">
-  <!-- TODO: hack club webring´® -->
-  <div class="flex gap-4">
-    <a class="hover:underline" target="_blank" href="https://github.com/deltea">github</a>
-    <a class="hover:underline" target="_blank" href="https://deltea.itch.io">itch.io</a>
-    <!-- <a class="hover:underline" target="_blank" href="https://github.com/deltea/dotfiles">dotfiles</a> -->
-    <!-- <a target="_blank" href="mailto:hello@deltea.space">contact</a> -->
-    <a class="hover:underline" href="/guestbook">guestbook</a>
-  </div>
-
+  <pagering-link theme="dark"></pagering-link>
   <span class="font-normal text-muted sm:block hidden">made with sveltekit</span>
 </footer>
